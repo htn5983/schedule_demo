1b5ef8dea220424b4e720da5b71e95c68cf452a8
htn5983 <a0925072598@gmail.com>
2026-08-24 15:17:38 +0800

    加入自動停止(clear)的排程

--- src/time_demo/deco_demo.py
+++ src/time_demo/deco_demo.py
@@ -1,5 +1,5 @@
 #使用 Decorator / Annotation 方式宣告
-from schedule import every, repeat, run_pending
+from schedule import every, repeat, run_pending, clear, get_jobs
 from datetime import datetime
 import time
 
@@ -11,7 +11,7 @@ def task1():
 def task2():
     print(f"這是任務  2 {datetime.now()}")
 
-@repeat(every(20).seconds.until("13:48"))
+@repeat(every(20).seconds.until("15:30"))
 def task3():
     print(f"這是任務  3 {datetime.now()}")
 
@@ -19,7 +19,17 @@ def task3():
 def task4():
     print(f"*** 這是亂數任務 4 {datetime.now()}")
 
+@repeat(every(30).seconds)
+def task5():
+    clear()
 
 while True:
     run_pending()
+    print(f"job pool 是否有任務: {len(get_jobs())}")
+    if len(get_jobs()) == 0:
+        print("job pool 內已無任何任務可執行")
+        break   #離開while迴圈，因為已無任務
     time.sleep(1)
+
+
+print("程式結束")
